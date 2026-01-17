c49d79c3f10d2550edf18cb0a610f1987c18f58c
Sagee <johnashgames23@gmail.com>
2026-01-17 22:53:22 +0800

    feat(auth): integrate AuthProvider
    - Make auth context globally available
    - Prepare app for authentication logic
    - Enable role-based access control

--- Role-Based-Auth/src/App.tsx
+++ Role-Based-Auth/src/App.tsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "@tanstack/react-router";
 import router from "./router";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
     <>
-     <RouterProvider router={router}/>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
+
     </>
   );
 }
