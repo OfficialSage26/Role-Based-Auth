364eb5e10f9af6748e7ac5dbfef7027832d6cec1
Sagee <johnashgames23@gmail.com>
2026-01-18 16:08:04 +0800

    feat(dashboard): access authenticated user

--- Role-Based-Auth/src/routes/_protected/dashboard.tsx
+++ Role-Based-Auth/src/routes/_protected/dashboard.tsx
@@ -1,9 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useAuth } from '../../context/useAuth'
 
 export const Route = createFileRoute('/_protected/dashboard')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const {user} = useAuth();
+  console.log(user);
   return <div>Hello "/_protected/dashboard"!</div>
 }
