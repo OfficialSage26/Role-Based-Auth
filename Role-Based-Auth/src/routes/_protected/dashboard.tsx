8409ae9b21a6dcf8261e2dbef8d7597d2b8c6e33
Sagee <johnashgames23@gmail.com>
2026-01-18 16:23:13 +0800

    fix(dashboard): Handle missing user data in view

--- Role-Based-Auth/src/routes/_protected/dashboard.tsx
+++ Role-Based-Auth/src/routes/_protected/dashboard.tsx
@@ -10,8 +10,17 @@ function RouteComponent() {
   console.log(user);
   return (
     <div className="p-10">
-      <h2 className="text-4xl font-bold mb-2">Welcome, {user?.username}</h2>
-      <p className="text-gray-800">Role: {user?.role}</p>
+      {user ? (
+        <>
+          <h2 className="text-4xl font-bold mb-2">Welcome, {user?.username}</h2>
+          <p className="text-gray-800">Role: {user?.role}</p>
+        </>
+      ) : (
+        <>
+          <h2 className="text-4xl font-bold mb-2">Welcome, Guest</h2>
+          <p className="text-gray-800">No assigned role to you</p>
+        </>
+      )}
     </div>
   );
 }
